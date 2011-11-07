c530061f63590ce327795111ef56d9d154789cbd
Ertan Dogrultan <e.dogrultan@gmail.com>
2011-11-06 16:53:24 -0800

    committing the RoR skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SurveyFrameworkRoR::Application.config.secret_token = '2a021acc3330e6021fc0ca81553d4d422dbd5af0115986578e61e04c1a14055efd3b7105b5420b1464abc465c34fb195ecdee2b7fd194d2d7d74bfc3cc044d49'
